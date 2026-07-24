cbd3697ebce6d27b528862b77580c82121764333
mathieu moutault <mathieu.moutault@orange.com>
2026-07-24 12:50:01 +0200

    commit

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -12,9 +12,17 @@ function searchRecommendations(){
             if(!country) {
                 const cities = data.countries.flatMap(item => item.cities);
                 city = cities.find(item => item.name.toLowerCase().includes(input));
-                displayResult(city);
+            } 
+
+            let temple = data.temples.find(item => item.name.toLowerCase().includes(input));
+            let beach = data.beaches.find(item => item.name.toLowerCase().includes(input));
+
+            // Find the first result that exists
+            const result = country || city || temple || beach;
+
+            if(result) {
+                displayResult(result);
             }
-            console.log(country,city);
         })
         .catch(error => {
 			console.error('Error:', error);
@@ -30,4 +38,5 @@ function displayResult(result){
     const resultDiv = document.getElementById("recommendationsResult");
     console.log(result.map);
     resultDiv.innerHTML += `<p>${result.name}: ${result.description}</p>`;
+    resultDiv.innerHTML += `<img src="${result.imageUrl}" alt="${result.name}">`;
 }

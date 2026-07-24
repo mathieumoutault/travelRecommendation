278c351fc5a5599383d7140e1f94f6423d6b5987
mathieu moutault <mathieu.moutault@orange.com>
2026-07-24 11:59:01 +0200

    commit

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,11 +1,33 @@
 function searchRecommendations(){
+    const input=document.getElementById("searchRecommendation").value.toLowerCase();
+    const resultDiv = document.getElementById("recommendationsResult");
+    resultDiv.innerHTML ='';
+    console.log(input);
 
+    fetch('travel_recommendation_api.json')
+        .then(response => response.json())
+        .then(data => {
+            let country = data.countries.find(item => item.name.toLowerCase().includes(input));
+            let city;
+            if(!country) {
+                const cities = data.countries.flatMap(item => item.cities);
+                city = cities.find(item => item.name.toLowerCase().includes(input));
+                displayResult(city);
+            }
+            console.log(country,city);
+        })
+        .catch(error => {
+			console.error('Error:', error);
+			resultDiv.innerHTML = 'An error occurred while fetching data.';
+        });
 }
 
 function clearResult(){
-    document.getElementById("result").innerHTML="";  
+    document.getElementById("recommendationsResult").innerHTML="";  
 }
 
-function displayResult(){
-    
+function displayResult(result){
+    const resultDiv = document.getElementById("recommendationsResult");
+    console.log(result.map);
+    resultDiv.innerHTML += `<p>${result.name}: ${result.description}</p>`;
 }
